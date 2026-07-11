20b232bd45f7ede003ddad54b5a320658ace013f
Prodeon <prodeon21@gmail.com>
2026-07-11 15:22:26 +0300

    add include from users.urls in config urls

--- config/urls.py
+++ config/urls.py
@@ -20,6 +20,7 @@ from django.urls import path, include
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+from users.apps import UsersConfig
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -38,4 +39,7 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+
+    path("users/", include("users.urls", namespace=UsersConfig.name)),
+
 ]
