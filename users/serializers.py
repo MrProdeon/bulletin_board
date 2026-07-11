db1bb05dde7889430f5ebc1989b37be4cb67d90f
Prodeon <prodeon21@gmail.com>
2026-07-11 15:23:43 +0300

    add CustomUserSerializer with create method

---
+++ users/serializers.py
@@ -0,0 +1,27 @@
+from rest_framework.serializers import ModelSerializer
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from users.models import CustomUser
+
+class CustomUserSerializer(ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = "__all__"
+
+    def create(self, validated_data):
+        password = validated_data.pop("password")
+        email = validated_data.pop("email")
+
+        user = CustomUser.objects.create_user(
+            email=email, password=password, **validated_data
+        )
+        return user
+
+# class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+#     @classmethod
+#     def get_token(cls, user):
+#         token = super().get_token(user)
+#
+#         token['username'] = user.username
+#         token['email'] = user.email
+#
+#         return token
