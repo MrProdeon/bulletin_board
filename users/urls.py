37c86d5b21e9081e7675c05d81cddccff31f6771
Prodeon <prodeon21@gmail.com>
2026-07-11 15:31:32 +0300

    add url for refresh token

--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from rest_framework.urls import app_name
+from rest_framework_simplejwt.views import TokenRefreshView
 from users.apps import UsersConfig
 from users.views import UserCreateAPIView, MyTokenObtainPairView
 
@@ -8,5 +9,6 @@ app_name = UsersConfig.name
 urlpatterns = [
     path("register/", UserCreateAPIView.as_view(), name="register"),
     path('token/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path("refresh_token/", TokenRefreshView.as_view(), name="refresh-token"),
 
 ]
