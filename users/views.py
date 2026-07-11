b36b319307af6f36a38c19299c9e9f764de17b94
Prodeon <prodeon21@gmail.com>
2026-07-11 15:23:18 +0300

    add APIView for register user

--- users/views.py
+++ users/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from rest_framework.generics import CreateAPIView
+from rest_framework_simplejwt.views import TokenObtainPairView
 
-# Create your views here.
+from users.models import CustomUser
+from users.serializers import CustomUserSerializer
+
+
+class UserCreateAPIView(CreateAPIView):
+    queryset = CustomUser.objects.all()
+    serializer_class = CustomUserSerializer
+
+
+# class MyTokenObtainPairView(TokenObtainPairView):
+#     serializer_class = MyTokenObtainPairSerializer
