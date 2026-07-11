21f3fe82f22edb74c799b42b1215fe5d124af32b
Prodeon <prodeon21@gmail.com>
2026-07-11 14:32:54 +0300

    add settings for simple jwt

--- config/settings.py
+++ config/settings.py
@@ -139,3 +139,14 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
 AUTH_USER_MODEL = "users.CustomUser"
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ],
+}
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+}
