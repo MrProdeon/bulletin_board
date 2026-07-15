410e22749a106dc5261381dd6757ac7f0b74dd97
Prodeon <prodeon21@gmail.com>
2026-07-15 10:58:40 +0300

    add endpoints with viewset for reviews

--- reviews/views.py
+++ reviews/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from reviews.models import Review
+from reviews.serializers import ReviewSerializer
+
 
 # Create your views here.
+
+class ReviewViewSet(ModelViewSet):
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
