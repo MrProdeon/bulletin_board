c9317c4daa500d84358ca1f7c7317909a067db91
Prodeon <prodeon21@gmail.com>
2026-07-15 10:32:34 +0300

    add router for MyUserViewSet

--- users/urls.py
+++ users/urls.py
@@ -2,13 +2,19 @@ from django.urls import path
 from rest_framework.urls import app_name
 from rest_framework_simplejwt.views import TokenRefreshView
 from users.apps import UsersConfig
+from rest_framework.routers import DefaultRouter
 #from users.views import UserCreateAPIView, MyTokenObtainPairView
 
 app_name = UsersConfig.name
+
+router = DefaultRouter()
+router.register(r"", MyUserViewSet, basename="user")
 
 urlpatterns = [
     # path("register/", UserCreateAPIView.as_view(), name="register"),
     # path('login/', MyTokenObtainPairView.as_view(), name='login'),
     # path("refresh_token/", TokenRefreshView.as_view(), name="refresh-token"),
 
-]
+
+] + router.urls
+
