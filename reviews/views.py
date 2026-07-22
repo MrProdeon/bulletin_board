5457f32e2304a59edf13b28dceefaa2404b07b0f
Prodeon <prodeon21@gmail.com>
2026-07-22 10:40:24 +0300

    add permissions for reviews

--- reviews/views.py
+++ reviews/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from rest_framework.viewsets import ModelViewSet
+
+from permissions import IsOwnerOrAdmin
 from reviews.models import Review
 from reviews.serializers import ReviewSerializer
 
@@ -9,3 +11,12 @@ from reviews.serializers import ReviewSerializer
 class ReviewViewSet(ModelViewSet):
     queryset = Review.objects.all()
     serializer_class = ReviewSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+    def get_permissions(self):
+        if self.action in ["retrieve", "create", "list"]:
+            return [IsAuthenticated()]
+        elif self.action in ["update", "partial_update", "destroy"]:
+            return [IsAuthenticated(), IsOwnerOrAdmin()]
