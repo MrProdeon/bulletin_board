0f93340cc5e30e6b745f793a956c054be31d9e42
Prodeon <prodeon21@gmail.com>
2026-08-01 12:02:06 +0300

    add perms IsAuthenticated in MyUserViewSet

--- users/views.py
+++ users/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from rest_framework.generics import CreateAPIView
+from rest_framework.permissions import IsAuthenticated
 from rest_framework_simplejwt.views import TokenObtainPairView
 from rest_framework import viewsets
 
@@ -19,6 +20,11 @@ from users.serializers import CustomUserSerializer
 class MyUserViewSet(viewsets.ModelViewSet):
     queryset = CustomUser.objects.all()
     serializer_class = CustomUserSerializer
+
+    def get_permissions(self):
+        return [IsAuthenticated()]
+
+
 
 
 
